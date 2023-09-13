142de8014b08b2e1b9ebdbca740298def5684117
Ernesto8816 <144951637+Ernesto8816@users.noreply.github.com>
2023-09-13 13:28:40 -0600

    Add files via upload

---
+++ quform/emails/autoreply.php
@@ -0,0 +1,19 @@
+<?php if (!defined('QUFORM_ROOT')) exit; ?><html>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0">
+<img src="http://fixit.com.mx/archimex/img/archimex-logo-600.jpg" alt="Archimex" width="300">
+<h1>Archimex</h1>
+<table width="100%" cellpadding="0" cellspacing="0" border="0">
+    <tr>
+        <td valign="top" style="padding: 25px;"><table width="600" cellpadding="0" cellspacing="0" border="0" style="font: 14px Helvetica, Arial, sans-serif;">
+            <tr>
+                <td valign="top" style="font-family: Helvetica, Arial, sans-serif; font-size: 25px; font-weight: bold; color: #282828; padding-bottom: 10px;"><?php echo Quform::escape($mailer->Subject); ?></td>
+            </tr>
+            <tr>
+                <td valign="top">Uno de nuestros asesores estará en contacto contigo.</td>
+            </tr>
+        </table></td>
+    </tr>
+</table>
+<br>
+</body>
+</html>
